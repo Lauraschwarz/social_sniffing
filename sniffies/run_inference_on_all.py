7cab66a2936c1be2578929a833959f26fe00ae09
Lauraschwarz <lau.schwarz@t-online.de>
2025-07-17 10:01:58 +0100

    run_inference file formated and refactored

--- sniffies/run_inference_on_all.py
+++ sniffies/run_inference_on_all.py
@@ -1,7 +1,35 @@
 import subprocess
 import pathlib
+
+#your source directory where the video files are stored
 ROOT_FOLDER = "F:/social_sniffing/rawdata"
+#your output directory where the inference results will be saved
 OUTPUT_FOLDER = "F:/social_sniffing/derivatives/"
+
+
+"""
+this is a quick script that allows you to run your Sleap inference on video files in a directory by calling subprocess. 
+It will create a new folder in the OUTPUT_FOLDER with the same structure as the ROOT_FOLDER and save the inference results there.
+it will save a .slp file but also an .h5 file for further analysis for example with movement (https://movement.neuroinformatics.dev/index.html)
+
+
+-------
+Parameters:
+ROOT_FOLDER: the folder where your video files are stored
+OUTPUT_FOLDER: the folder where you want to save the inference results
+
+
+call_inference()
+params:
+fpath: the path to the video file you want to run inference on
+dest_folder: the folder where you want to save the inference results
+command_inf: the command to run the inference, you need to change the model paths to your own models
+and potentially adjust inference parameters.
+when you want to run this script you need to have sleap installed in the environment where you run this script. 
+then just open a command line terminal activate the environment and type: python run_inference_on_all.py
+--------
+"""
+
 
 def call_inference_on_all(dest_folder=OUTPUT_FOLDER, ext=".avi"):
     source_folder = pathlib.Path(ROOT_FOLDER)
@@ -21,7 +49,6 @@ def call_inference(fpath, dest_folder):
 
     if fpath.exists():
         print(f"processing: {fpath}")
-        #command_inf = f"sleap-track -m C:\\Users\\Laura\\PycharmProjects\\sleap\\models\\new_tracks_lesions240708_162312.centroid.n=1201  -m C:\\Users\\Laura\\PycharmProjects\\sleap\\models\\new_tracks_lesions240708_170141.multi_class_topdown.n=1201  -o {dest_path}  --tracking.tracker none {fpath}  "
         command_inf = f"sleap-track -m F:\\sleap_thermistor\\models\\250626_154950.centroid.n=737  -m F:\\sleap_thermistor\\models\\250626_164214.multi_class_topdown.n=737  -o {dest_path}  --tracking.tracker none {fpath}  "
         print(f"{command_inf}")
         subprocess.call(command_inf, shell=True)
@@ -30,24 +57,7 @@ def call_inference(fpath, dest_folder):
         subprocess.call(command_conv, shell=True)
 
     else:
-        print(f'file: {fpath} does not exist; please check your input')
+        raise FileNotFoundError(f"File {fpath} does not exist. Please check your input.")
 
-def convert_inference_file(fpath,dest_folder=OUTPUT_FOLDER):
-    fpath = pathlib.Path(fpath)
-    dest_folder = pathlib.Path(dest_folder)
-    dest_path = dest_folder / f"{fpath.stem}.h5"
-    if fpath.exists():
-        print(f"converting: {fpath}")
-        command = f"sleap-convert --format analysis -o {dest_path} {fpath} "
-        print(f"{command}")
-        subprocess.call(command, shell=True)
-
-    else:
-        print(f'file: {fpath} does not exist; please check your input')
-def convert_all(dest_folder=OUTPUT_FOLDER, ext=".slp"):
-    source_folder = pathlib.Path(ROOT_FOLDER)
-    fpaths = list(source_folder.rglob(f"*{ext}"))
-    for fpath in fpaths:
-        call_inference(fpath, dest_folder)
-    return fpaths
-call_inference(fpath=pathlib.Path(r'F:\social_sniffing\rawdata\1125131\2025-07-01T12-47-28\Video\1125131.avi'), dest_folder=pathlib.Path(r'F:\social_sniffing\derivatives\1125131\2025-07-01T12-47-28\Video'))
+if __name__ == "__main__":
+    call_inference_on_all()
